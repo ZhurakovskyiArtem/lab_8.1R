a0de735053698122781dc05d97e7a64162519c7e
Artem <zhurakovskyiartem@gmail.com>
2021-11-24 17:56:49 +0200

    lab_8.1R

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8.1R/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
